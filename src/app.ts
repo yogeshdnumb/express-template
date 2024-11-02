7329516580218940e526c164cf10dd4cbe49c258
yogeshdnumb <dyogesh.official@gmail.com>
2024-11-02 22:30:07 +0530

    undo cookie secure

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ app.use(
   session({
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      secure: process.env.NODE_ENV === "production",
-      httpOnly: true,
+      // secure: process.env.NODE_ENV === "production",
+      // httpOnly: true,
     },
     secret: "cookie session secret",
     resave: false,
